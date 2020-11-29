f9052aaf45dddf3499b6dd9a295dcb3ba833cb96
Kurniaaasari <38637788+Kurniaaasari@users.noreply.github.com>
2020-11-29 23:38:48 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Lokasi from './Tugas/Lokasi';
+
+function App() {
+  return (
+      <div className="App-header">
+        <Lokasi/>
+      </div>
+  );
+}
+
+export default App;
